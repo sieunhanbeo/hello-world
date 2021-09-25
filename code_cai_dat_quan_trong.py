8106e33f3f91c1c34ea01b7990db2fc0dc47bbb1
sieunhanbeo <luanbkfet@gmail.com>
2021-09-25 16:03:09 +0700

    Update code_cai_dat_quan_trong.py

--- code_cai_dat_quan_trong.py
+++ code_cai_dat_quan_trong.py
@@ -6,3 +6,10 @@ python -m venv tenmoitruong
 tenmoitruong\scripts\activate.bat (active tren window)
 4. deactive (thoat)
 5. Python tenfile.py (Cd toi thu muc chua file tenfile.py nay da)
+6. Deploy app github - azure
+https://docs.microsoft.com/en-us/azure/app-service/quickstart-python?tabs=bash&pivots=python-framework-flask
+7. Xoa 1 thu muc trong linux https://vi.joecomp.com/how-remove-directory-linux
+8. Deploy tu github sang Azure 
+https://docs.microsoft.com/en-us/azure/app-service/quickstart-python?tabs=bash&pivots=python-framework-flask
+9. Autodepployment CICD
+https://github.blog/2015-09-15-automating-code-deployment-with-github-and-azure/
