be8bc6958c13dc535b75c53e23c4be0697b246a6
sieunhanbeo <91359527+sieunhanbeo@users.noreply.github.com>
2021-09-25 16:30:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! I am using GIT. Let's start"
+    return "Hello World! I am using GIT. Let's start. haha"
 
 if __name__ == '__main__':
     app.run(debug =True)
