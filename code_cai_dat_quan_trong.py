0a74bf29914dc20117fa8ec15f2a4983cfb16331
sieunhanbeo <luanbkfet@gmail.com>
2021-09-25 13:47:48 +0700

    Create code_cai_dat_quan_trong.py

---
+++ code_cai_dat_quan_trong.py
@@ -0,0 +1,7 @@
+1. install Python tren trang python
+2. install jupyter : 
+pip install jupyter
+3. tao ra 1 moi truong ao moi
+python -m venv tenmoitruong
+tenmoitruong\scripts\activate.bat (active tren window)
+deactive (thoat)
